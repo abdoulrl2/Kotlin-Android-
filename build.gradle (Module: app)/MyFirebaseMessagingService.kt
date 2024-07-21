7f66b46d7d5590f10d8628094a97fa3d93a6e6b2
abdoulrl2 <abdoulrl2025@gmail.com>
2024-07-21 15:21:34 -0300

    Create MyFirebaseMessagingService.kt
    Crie um serviço para receber notificações

---
+++ build.gradle (Module: app)/MyFirebaseMessagingService.kt
@@ -0,0 +1,11 @@
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import android.util.Log
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+        Log.d("FCM", "From: ${remoteMessage.from}")
+        Log.d("FCM", "Message Body: ${remoteMessage.notification?.body}")
+    }
+}
